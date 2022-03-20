2bc523cc5063f9bdf2df8f91a28b39ea820c9cdf
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-03-20 13:19:17 +0530

    Created the sorting repo

---
+++ Src/src/Bolier_Plate_code/Main.java
@@ -0,0 +1,7 @@
+package Bolier_Plate_code;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello sorting in linear and binary search !! ");
+    }
+}
