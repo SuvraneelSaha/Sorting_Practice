c53beb45806d9cb5e3a89caedc935da55a030422
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-03-22 00:30:31 +0530

    bug

--- Src/src/ImportantQuestions/Ceiling_Reversal.java
+++ Src/src/ImportantQuestions/Ceiling_Reversal.java
@@ -1,7 +1,53 @@
 package ImportantQuestions;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+// the arrays is arranged in the reverse order ie in descending order
 public class Ceiling_Reversal {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("please enter the size of the array :");
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        System.out.println("please enter the elements in the array:");
+        for (int i = 0; i <arr.length ; i++)
+        {
+            arr[i] = in.nextInt();
+        }
+        System.out.println("OG array--> ");
+        System.out.println(Arrays.toString(arr));
+
+        System.out.println("please enter the target element :");
+        int target = in.nextInt();
+        System.out.printf("the ceiling element with respect to the target element %d is %d ",target,ceiling_reverse(arr,target));
 
     }
+    static int ceiling_reverse(int[] arr , int target)
+    {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+
+            if (target > arr[mid]) {
+                end = mid -1 ;
+            } else if (target < arr[mid]) {
+                start = mid + 1 ;
+            } else {
+                return mid;
+            }
+
+        }
+        return end; // cause ceiling in reverse order
+
+
+    }
+
+    // dont get confused about the ceiling and the floor
+    // in normal sorted array
+    // ceiling ---> start pointer
+    // floor ---> end pointer
+
 }
--- Src/src/ImportantQuestions/Order_Agnostic_Floor.java
+++ Src/src/ImportantQuestions/Order_Agnostic_Floor.java
@@ -3,9 +3,7 @@ package ImportantQuestions;
 import java.util.Arrays;
 import java.util.Scanner;
 
-// the array is arranged in descending order
-// and we have to dinf the floor element according to the target element
-public class Floor_Reversal {
+public class Order_Agnostic_Floor {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("please enter the size of the array :");
@@ -21,34 +19,48 @@ public class Floor_Reversal {
 
         System.out.println("please enter the target element :");
         int target = in.nextInt();
-        System.out.printf("the floor element with respect to the target element %d is %d ",target,floor_reverse(arr,target));
-
-
+        System.out.printf("the floor element with respect to the target element %d is %d ",target,order_agnostic_floor(arr,target));
     }
-    static int floor_reverse(int[] arr , int target) // these are known as parameters and the
-            // values that are passed are known as arguments
 
-            // as the array is reversed soo
-    // ex - 55 44 33 22 11 ; t - 25 floor - 22
-    {
+    private static int  order_agnostic_floor(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
-        while (start <= end)
+
+        boolean isasc ;
+        if(arr[0] < arr[end])
         {
+            isasc = true;
+        }
+
+        if(isasc)
+        {
+        while (start <= end) {
             int mid = start + (end - start) / 2;
 
+
             if (target > arr[mid]) {
-                end = mid -1 ;
+                start = mid + 1;
             } else if (target < arr[mid]) {
-                start = mid + 1 ;
+                end = mid - 1;
             } else {
                 return mid;
             }
 
         }
-        return start; // cause floor
+            else
+            {
+                if (target > arr[mid]) {
+                    end = mid -1 ;
+                } else if (target < arr[mid]) {
+                    start = mid + 1 ;
+                } else {
+                    return mid;
+                }
+            }
 
+
+
+        }
+        return start;
     }
-    // ex - 55 44 33 22 11 ; t - 25 floor - 22
-
 }
