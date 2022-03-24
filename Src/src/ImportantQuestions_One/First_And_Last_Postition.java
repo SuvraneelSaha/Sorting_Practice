a9eb1b954b1706ed824c171420ab890fc49573a3
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-03-24 23:55:24 +0530

    Update First_And_Last_Postition.java

--- Src/src/ImportantQuestions_One/First_And_Last_Postition.java
+++ Src/src/ImportantQuestions_One/First_And_Last_Postition.java
@@ -1,5 +1,8 @@
 package ImportantQuestions_One;
 // Question link : https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+// the duplicate elements will be allowed
+// and the first starting target element will be the the ans
+// also the 2nd target will be the ans also
 
 
 
